90c2545d48ea38b5b4e812cfd07b99c4ba374332
manishbisht12 <154014370+manishbisht12@users.noreply.github.com>
2024-06-27 23:41:16 +0530

    Add files via upload

---
+++ p5.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main() {
+    int number;
+
+    // Prompt the user to enter a number
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    // Check if the number is even or odd
+    if (number % 2 == 0) {
+        printf("%d is an even number.\n", number);
+    } else {
+        printf("%d is an odd number.\n", number);
+    }
+
+    return 0;
+}
